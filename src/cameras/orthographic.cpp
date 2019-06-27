de711869abf4b21d6928432ac20c9d3013e0bfdc
Backes Thierry <backes.thierry@gmail.com>
2019-06-27 17:31:16 +0200

    added GenerateRay to orthographic camera

--- src/cameras/orthographic.cpp
+++ src/cameras/orthographic.cpp
@@ -6,8 +6,12 @@
 #include "sampler.h"
 
 namespace pbrt {
-    float OrthographicCamera::GenerateRay(const pbrt::CameraSample &sample, pbrt::Ray *) const {
-        return 0;
+    float OrthographicCamera::GenerateRay(const pbrt::CameraSample &sample, pbrt::Ray * ray) const {
+        Point3f pFilm = Point3f(sample.pFilm.x, sample.pFilm.y, 0);
+        Point3f pCamera = RasterToCamera(pFilm);
+        *ray = Ray(pCamera, Vector3f(0, 0, 1));
+        *ray = CameraToWorld(*ray);
+        return 1;
     }
 
 }
--- src/core/transform.h
+++ src/core/transform.h
@@ -47,8 +47,13 @@ namespace pbrt{
             friend Transform Inverse(const Transform &t) {
                 return Transform(t.mInv, t.m);
             }
+
+            template <typename T>
+            inline Point3<T> operator()(const Point3<T> &p) const;
             template <typename T>
             inline Vector3<T> operator()(const Vector3<T> &v) const;
+            inline Ray operator()(const Ray &r) const;
+
             Transform operator*(const Transform &t2) const;
 
         private:
@@ -61,6 +66,18 @@ namespace pbrt{
     Transform LookAt(const Point3f &pos, const Point3f &look, const Vector3f &up);
     Transform Orthographic(float znear, float zfar);
 
+    template <typename T>
+    inline Point3<T> Transform::operator()(const Point3<T> &p) const {
+        T x = p.x, y = p.y, z = p.z;
+        T xp = m.m[0][0] * x + m.m[0][1] * y + m.m[0][2] * z + m.m[0][3];
+        T yp = m.m[1][0] * x + m.m[1][1] * y + m.m[1][2] * z + m.m[1][3];
+        T zp = m.m[2][0] * x + m.m[2][1] * y + m.m[2][2] * z + m.m[2][3];
+        T wp = m.m[3][0] * x + m.m[3][1] * y + m.m[3][2] * z + m.m[3][3];
+        if (wp == 1)
+            return Point3<T>(xp, yp, zp);
+        else
+            return Point3<T>(xp, yp, zp) / wp;
+    }
 
     template <typename T>
     inline Vector3<T> Transform::operator()(const Vector3<T> &v) const {
@@ -68,6 +85,20 @@ namespace pbrt{
         return Vector3<T>(m.m[0][0] * x + m.m[0][1] * y + m.m[0][2] * z,
                           m.m[1][0] * x + m.m[1][1] * y + m.m[1][2] * z,
                           m.m[2][0] * x + m.m[2][1] * y + m.m[2][2] * z);
+    }
+
+    inline Ray Transform::operator()(const Ray &r) const {
+        Point3f o = (*this)(r.o);
+        Vector3f d = (*this)(r.d);
+        // Offset ray origin to edge of error bounds and compute _tMax_
+        float lengthSquared = d.LengthSquared();
+        float tMax = r.tMax;
+        if (lengthSquared > 0) {
+            float dt = Dot(Abs(d)) / lengthSquared;
+            o += d * dt;
+            tMax -= dt;
+        }
+        return Ray(o, d, tMax);
     }
 }
 #endif //PBRT_WHITTED_TRANSFORM_H

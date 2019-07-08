6e1fd1f3a71822cb6cd6cf9b8df704a8d8b68a83
Backes Thierry <backes.thierry@gmail.com>
2019-07-09 00:02:54 +0200

    Added SpecularReflect and SpecularTransmit

--- src/core/integrator.cpp
+++ src/core/integrator.cpp
@@ -92,4 +92,90 @@ namespace pbrt{
         }
     }
 
+    Spectrum
+    SamplerIntegrator::SpecularReflect(const RayDifferential &ray, const SurfaceInteraction &isect, const Scene &scene,
+                                       Sampler &sampler, MemoryArena &arena, int depth) const {
+        // Compute specular reflection direction _wi_ and BSDF value
+        Vector3f wo = isect.wo, wi;
+        float pdf;
+        BxDFType type = BxDFType(BSDF_REFLECTION | BSDF_SPECULAR);
+        Spectrum f = isect.bsdf->Sample_f(wo, &wi, sampler.Get2D(), &pdf, type);
+        // Return contribution of specular reflection
+        const Normal3f &ns = isect.shading.n;
+        if (pdf > 0.f && !f.IsBlack() && AbsDot(wi, ns) != 0.f) {
+            // Compute ray differential _rd_ for specular reflection
+            RayDifferential rd = isect.SpawnRay(wi);
+            if (ray.hasDifferentials) {
+                rd.hasDifferentials = true;
+                rd.rxOrigin = isect.p + isect.dpdx;
+                rd.ryOrigin = isect.p + isect.dpdy;
+                // Compute differential reflected directions
+                Normal3f dndx = isect.shading.dndu * isect.dudx +
+                                isect.shading.dndv * isect.dvdx;
+                Normal3f dndy = isect.shading.dndu * isect.dudy +
+                                isect.shading.dndv * isect.dvdy;
+                Vector3f dwodx = -ray.rxDirection - wo,
+                        dwody = -ray.ryDirection - wo;
+                float dDNdx = Dot(dwodx, ns) + Dot(wo, dndx);
+                float dDNdy = Dot(dwody, ns) + Dot(wo, dndy);
+                rd.rxDirection =
+                        wi - dwodx + 2.f * Vector3f(Dot(wo, ns) * dndx + dDNdx * ns);
+                rd.ryDirection =
+                        wi - dwody + 2.f * Vector3f(Dot(wo, ns) * dndy + dDNdy * ns);
+            }
+            return f * Li(rd, scene, sampler, arena, depth + 1) * AbsDot(wi, ns) /
+                   pdf;
+        } else
+            return Spectrum(0.f);
+    }
+
+    Spectrum
+    SamplerIntegrator::SpecularTransmit(const RayDifferential &ray, const SurfaceInteraction &isect, const Scene &scene,
+                                        Sampler &sampler, MemoryArena &arena, int depth) const {
+        Vector3f wo = isect.wo, wi;
+        float pdf;
+        const Point3f &p = isect.p;
+        const Normal3f &ns = isect.shading.n;
+        const BSDF &bsdf = *isect.bsdf;
+        Spectrum f = bsdf.Sample_f(wo, &wi, sampler.Get2D(), &pdf,
+                                   BxDFType(BSDF_TRANSMISSION | BSDF_SPECULAR));
+        Spectrum L = Spectrum(0.f);
+        if (pdf > 0.f && !f.IsBlack() && AbsDot(wi, ns) != 0.f) {
+            // Compute ray differential _rd_ for specular transmission
+            RayDifferential rd = isect.SpawnRay(wi);
+            if (ray.hasDifferentials) {
+                rd.hasDifferentials = true;
+                rd.rxOrigin = p + isect.dpdx;
+                rd.ryOrigin = p + isect.dpdy;
+
+                float eta = bsdf.eta;
+                Vector3f w = -wo;
+                if (Dot(wo, ns) < 0) eta = 1.f / eta;
+
+                Normal3f dndx = isect.shading.dndu * isect.dudx +
+                                isect.shading.dndv * isect.dvdx;
+                Normal3f dndy = isect.shading.dndu * isect.dudy +
+                                isect.shading.dndv * isect.dvdy;
+
+                Vector3f dwodx = -ray.rxDirection - wo,
+                        dwody = -ray.ryDirection - wo;
+                float dDNdx = Dot(dwodx, ns) + Dot(wo, dndx);
+                float dDNdy = Dot(dwody, ns) + Dot(wo, dndy);
+
+                float mu = eta * Dot(w, ns) - Dot(wi, ns);
+                float dmudx =
+                        (eta - (eta * eta * Dot(w, ns)) / Dot(wi, ns)) * dDNdx;
+                float dmudy =
+                        (eta - (eta * eta * Dot(w, ns)) / Dot(wi, ns)) * dDNdy;
+
+                rd.rxDirection =
+                        wi + eta * dwodx - Vector3f(mu * dndx + dmudx * ns);
+                rd.ryDirection =
+                        wi + eta * dwody - Vector3f(mu * dndy + dmudy * ns);
+            }
+            L = f * Li(rd, scene, sampler, arena, depth + 1) * AbsDot(wi, ns) / pdf;
+        }
+        return L;
+    }
+
 }
--- src/core/integrator.h
+++ src/core/integrator.h
@@ -31,6 +31,15 @@ namespace pbrt{
         virtual Spectrum Li(const RayDifferential &ray, const Scene &scene,
                             Sampler &sampler, MemoryArena &arena,
                             int depth = 0) const = 0;
+
+        Spectrum SpecularReflect(const RayDifferential &ray,
+                                 const SurfaceInteraction &isect,
+                                 const Scene &scene, Sampler &sampler,
+                                 MemoryArena &arena, int depth) const;
+        Spectrum SpecularTransmit(const RayDifferential &ray,
+                                  const SurfaceInteraction &isect,
+                                  const Scene &scene, Sampler &sampler,
+                                  MemoryArena &arena, int depth) const;
     protected:
         std::shared_ptr<const Camera> camera;
 

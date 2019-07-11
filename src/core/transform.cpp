75c3d03ecfbf6cc535fab0de28eda9fbfa40cae4
Backes Thierry <backes.thierry@gmail.com>
2019-07-11 22:16:43 +0200

    implemented Matrix inverse

--- src/core/transform.cpp
+++ src/core/transform.cpp
@@ -40,6 +40,62 @@ namespace pbrt{
         m[3][3] = t33;
     }
 
+    Matrix4x4 Inverse(const Matrix4x4 &m) {
+        int indxc[4], indxr[4];
+        int ipiv[4] = {0, 0, 0, 0};
+        float minv[4][4];
+        memcpy(minv, m.m, 4 * 4 * sizeof(float));
+        for (int i = 0; i < 4; i++) {
+            int irow = 0, icol = 0;
+            float big = 0.f;
+            // Choose pivot
+            for (int j = 0; j < 4; j++) {
+                if (ipiv[j] != 1) {
+                    for (int k = 0; k < 4; k++) {
+                        if (ipiv[k] == 0) {
+                            if (std::abs(minv[j][k]) >= big) {
+                                big = float(std::abs(minv[j][k]));
+                                irow = j;
+                                icol = k;
+                            }
+                        } else if (ipiv[k] > 1);
+
+                    }
+                }
+            }
+            ++ipiv[icol];
+            // Swap rows _irow_ and _icol_ for pivot
+            if (irow != icol) {
+                for (int k = 0; k < 4; ++k) std::swap(minv[irow][k], minv[icol][k]);
+            }
+            indxr[i] = irow;
+            indxc[i] = icol;
+            if (minv[icol][icol] == 0.f);
+
+            // Set $m[icol][icol]$ to one by scaling row _icol_ appropriately
+            float pivinv = 1. / minv[icol][icol];
+            minv[icol][icol] = 1.;
+            for (int j = 0; j < 4; j++) minv[icol][j] *= pivinv;
+
+            // Subtract this row from others to zero out their columns
+            for (int j = 0; j < 4; j++) {
+                if (j != icol) {
+                    float save = minv[j][icol];
+                    minv[j][icol] = 0;
+                    for (int k = 0; k < 4; k++) minv[j][k] -= minv[icol][k] * save;
+                }
+            }
+        }
+        // Swap columns to reflect permutation
+        for (int j = 3; j >= 0; j--) {
+            if (indxr[j] != indxc[j]) {
+                for (int k = 0; k < 4; k++)
+                    std::swap(minv[k][indxr[j]], minv[k][indxc[j]]);
+            }
+        }
+        return Matrix4x4(minv);
+    }
+
     Transform LookAt(const Point3f &pos, const Point3f &look, const Vector3f &up) {
         Matrix4x4 cameraToWorld;
         cameraToWorld.m[0][3] = pos.x;
